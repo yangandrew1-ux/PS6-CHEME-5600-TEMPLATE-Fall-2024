520a952003b929594a65e68a7715b386f2edc2f5
Jeffrey Varner <jdv27@cornell.edu>
2024-11-24 14:45:10 -0500

    Running
    Running

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# PS6-CHEME-5600-TEMPLATE-Fall-2024
+Template for Problem Set 6 for CHEME 5660 Fall 2024
---
+++ src/Files.jl
@@ -0,0 +1,6 @@
+function _jld2(path::String)::Dict{String,Any}
+    return load(path);
+end
+
+MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2018-to-12-29-2023.jld2"));
+MyOutOfSampleMarketDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-10-25-2024.jld2"));
